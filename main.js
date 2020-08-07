791f5c9aa13e1e181ae57946ac2a5ecde0277ed1
Marlon Berkenbrock <35845065+mberkenbrock@users.noreply.github.com>
2020-08-07 00:55:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -29,66 +29,77 @@ function apiCalcInss(valor){
     if(valor != "" && valor > 0) {
         if(valor >= acimaTeto){
             valorInss = vlTeto;
-            base1.innerHTML = valorFaixaDesconto[0];
-            valor1.innerHTML = valorFaixaDesconto[0] * percentualFaixa[0];
-            base2.innerHTML = valorFaixaDesconto[1];
-            valor2.innerHTML = valorFaixaDesconto[1] * percentualFaixa[1];
-            base3.innerHTML = valorFaixaDesconto[2];
-            valor3.innerHTML = valorFaixaDesconto[2] * percentualFaixa[2];
-            base4.innerHTML = valorFaixaDesconto[3];
-            valor4.innerHTML = valorFaixaDesconto[3] * percentualFaixa[3];
+            base1.innerHTML  = (valorFaixaDesconto[0] - 0.005).toFixed(2);
+            valor1.innerHTML = (valorFaixaDesconto[0] * percentualFaixa[0] - 0.005).toFixed(2);
+            base2.innerHTML  = (valorFaixaDesconto[1] - 0.005).toFixed(2);
+            valor2.innerHTML = (valorFaixaDesconto[1] * percentualFaixa[1] - 0.005).toFixed(2);
+            base3.innerHTML  = (valorFaixaDesconto[2] - 0.005).toFixed(2);
+            valor3.innerHTML = (valorFaixaDesconto[2] * percentualFaixa[2] - 0.005).toFixed(2);
+            base4.innerHTML  = (valorFaixaDesconto[3] - 0.005).toFixed(2);
+            valor4.innerHTML = (valorFaixaDesconto[3] * percentualFaixa[3] - 0.005).toFixed(2);
         }
         else{
             if(valor <= valorFaixaDesconto[0]){
                 valorInss = valor * percentualFaixa[0];
-                base1.innerHTML = valor;
-                valor1.innerHTML = valor * percentualFaixa[0];
+                base1.innerHTML =  valor.toFixed(2); 
+                valor1.innerHTML = (valor * percentualFaixa[0] - 0.005).toFixed(2);
             }
             else {     
                 valorRestante = valor - valorFaixaDesconto[0]; 
                 valorInss = faixaValorInss[0];
 
-                base1.innerHTML = valorFaixaDesconto[0];
-                valor1.innerHTML = valorFaixaDesconto[0] * percentualFaixa[0];
+                base1.innerHTML = valorFaixaDesconto[0].toFixed(2);
+                valor1.innerHTML = (valorFaixaDesconto[0] * percentualFaixa[0] - 0.005).toFixed(2);
 
                 if(valorRestante > valorFaixaDesconto[1]){
                     valorRestante -= valorFaixaDesconto[1]; 
                     valorInss += faixaValorInss[1];
 
-                    base2.innerHTML = valorFaixaDesconto[1];
-                    valor2.innerHTML = valorFaixaDesconto[1] * percentualFaixa[1];
+                    base2.innerHTML = valorFaixaDesconto[1].toFixed(2) ;
+                    valor2.innerHTML = (valorFaixaDesconto[1] * percentualFaixa[1] - 0.005).toFixed(2);
 
                     if(valorRestante > valorFaixaDesconto[2]){
                         valorRestante -= valorFaixaDesconto[2]; 
                         valorInss += faixaValorInss[2];
 
-                        base3.innerHTML = valorFaixaDesconto[2];
-                        valor3.innerHTML = valorFaixaDesconto[2] * percentualFaixa[2];
+                        base3.innerHTML = valorFaixaDesconto[2].toFixed(2);
+                        valor3.innerHTML = (valorFaixaDesconto[2] * percentualFaixa[2] - 0.005).toFixed(2);
 
                         if(valorRestante > valorFaixaDesconto[3]){
                             valorInss += faixaValorInss[3];
                             
-                            base4.innerHTML = valorFaixaDesconto[3];
-                            valor4.innerHTML = valorFaixaDesconto[3] * percentualFaixa[3];
+                            base4.innerHTML = valorFaixaDesconto[3].toFixed(2);
+                            valor4.innerHTML = (valorFaixaDesconto[3] * percentualFaixa[3] - 0.005).toFixed(2);
                         }
                         else{
                             valorInss += valorRestante * percentualFaixa[3];
-                            base3.innerHTML = valorRestante;
-                            valor3.innerHTML = valorRestante * percentualFaixa[3];
+                            base4.innerHTML = (valorRestante - 0.005).toFixed(2);
+                            valor4.innerHTML = (valorRestante * percentualFaixa[3] - 0.005).toFixed(2);
                         }
                     }
                     else {
                         valorInss += valorRestante * percentualFaixa[2];
-                        base2.innerHTML = valorRestante;
-                        valor2.innerHTML = valorRestante * percentualFaixa[2];
+                        base3.innerHTML = (valorRestante - 0.005).toFixed(2);
+                        valor3.innerHTML = (valorRestante * percentualFaixa[2] - 0.005).toFixed(2);
                     }
                 }
                 else{
                     valorInss += valorRestante * percentualFaixa[1];
+                    base2.innerHTML = (valorRestante - 0.005).toFixed(2);
+                    valor2.innerHTML = (valorRestante * percentualFaixa[0] - 0.005).toFixed(2);
+
                 }
             }
         }
-        calcInss.value = (valorInss - 0.005).toFixed(2);
+        let totalBase = 0;
+        let totalValor = 0;
+        
+        totalBase = base1.innerHTML + base2.innerHTML + base3.innerHTML + base4.innerHTML;
+
+        totalValor = valor1.innerHTML + valor2.innerHTML + valor3.innerHTML + valor4.innerHTML;
+        document.getElementById("tb").innerHTML = totalBase.toFixed(2);
+        document.getElementById("tb").innerHTML = totalValor.toFixed(2);
+        calcInss.value =   (valorInss - 0.005).toFixed(2);
         vlAliquota = valor < acimaTeto ? ((valorInss / valor) * 100).toFixed(2) : '14.00';
         aliquota.value = vlAliquota + '%';
     }
